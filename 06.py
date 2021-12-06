e8814fc2558696551b62b22ca706fb60888993c5
Rajiv Giridharagopal <rgiri@uw.edu>
2021-12-05 22:29:35 -0800

    Add files via upload

--- 06.py
+++ 06.py
@@ -32,32 +32,31 @@ def grow_in_place(df):
     
     return df
 
-days = 80
-fish, fish_frame = load_fish()
-
-for d in range(days):
+# Combined part 1 and 2 for simplicity
+for parts in [80, 256]:
     
-    fish = grow_in_place(fish_frame)
+    fish, fish_frame = load_fish()
+    for d in range(parts):
+        fish = grow_in_place(fish_frame)
+    print(sum(fish_frame.loc[0]))
 
-print(sum(fish_frame.loc[0]))
+# Redoing with deque as per a reddit suggestion
+from collections import deque
 
-# Part 2
-days = 256
-fish, fish_frame = load_fish()
+def load_fish_only():
+    base = r'C:/Users/Raj/OneDrive/UW Work/Coding and Signal Processing Work/Python/aoc_2021/'
+    f = open(base + r'06_input.txt')
+    fish = np.array([int(x) for x in f.read().rstrip().split(',')])
+    return fish
 
-for d in range(days):
-    grow_in_place(fish_frame)
+for parts in [80, 256]:
     
-print(sum(fish_frame.loc[0]))
+    fish = load_fish_only()
+    fish_que = deque([len(np.where(fish == x)[0]) for x in range(9)])
+    for d in range(parts):
+        _0 = fish_que.popleft()
+        fish_que.append(_0)
+        fish_que[6] += _0
+    print(sum(fish_que))
 
-# Part 1 old
-def grow(num):
     
-    num -= 1
-    newfish = np.where(num == -1)[0]
-    if newfish.any():
-        num = np.append(num, 8*np.ones(len(newfish)))
-    
-    num[np.where(num == -1)] = 6
-    
-    return num
