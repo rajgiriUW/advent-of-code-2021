bb702204e56d18c8e486413597691cd2e9e8a3c7
Rajiv Giridharagopal <rgiri@uw.edu>
2021-12-06 21:41:56 -0800

    Add files via upload

---
+++ 07.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 20:48:53 2021
+
+@author: Raj
+"""
+import numpy as np
+
+base = r'C:/Users/Raj/OneDrive/UW Work/Coding and Signal Processing Work/Python/aoc_2021/'
+f = open(base + r'07_input.txt')
+crabs = np.array([int(x) for x in f.read().rstrip().split(',')])
+
+# Part 1: could tell by looking at the list that median is the best
+print(sum(np.abs(crabs - np.median(crabs))))
+
+# Part 2
+def sum_to_end(num):
+    return sum(np.arange(num+1))
+
+mn = int(np.floor(crabs.mean()))
+
+print(sum(list(map(sum_to_end, np.abs(crabs - mn)))))
